07ec777339e9c64b0c8e5ab7e42240c6ce82ef07
Tiana Robinson <trobinson1097@gmail.com>
2024-04-08 12:36:40 -0700

    conditions and while loops

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// string greeting = @"Welcome to Thrown For a Loop
+// Your one-stop shop for used sporting equipment";
+
+// Console.WriteLine(greeting);
+// Console.WriteLine("Please choose an option:");
+
+
+// string response = Console.ReadLine();
+// Console.WriteLine(@$"You chose: {response}. 
+// Thank you for your input!");
+
+
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Please enter a product name: ");
+
+string response = Console.ReadLine().Trim();
+
+while (string.IsNullOrEmpty(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine().Trim();
+}
+
+Console.WriteLine($"You chose: {response}");
